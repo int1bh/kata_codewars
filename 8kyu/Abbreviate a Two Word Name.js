0279dd509993fcb3a46b87b9108d8826571d4688
Alexandr <alexandrdryagin@yandex.ru>
2020-04-22 10:00:39 +0300

    complete

---
+++ 8kyu/Abbreviate a Two Word Name.js
@@ -0,0 +1,25 @@
+/*
+Abbreviate a Two Word Name
+
+Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
+
+The output should be two capital letters with a dot separating them.
+
+It should look like this:
+
+Sam Harris => S.H
+
+Patrick Feeney => P.F
+*/
+
+function abbrevName(name){
+
+    let arrName = name.split(" ");
+    let newArr = arrName.map(function(i){
+        return i.slice(0,1);
+    });
+    return newArr.join('.').toUpperCase();
+
+}
+
+console.log(abbrevName("дрягин Александр"));
